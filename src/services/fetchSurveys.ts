764e23e97f88d4a4950f717444ad62e2f9da59df
Enmanuel Rodriguez <enmaundostres@gmail.com>
2025-01-04 12:16:35 -0400

    add integration logic with the api

---
+++ src/services/fetchSurveys.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export type Option = {
+    id: number,
+    name: string,
+    votes: number
+}
+
+export type Creator = {
+    id: number,
+    nickname: string
+    image: String
+}
+
+export type Survey = {
+    id: number,
+    title: String,
+    creator: Creator,
+    options: Option[]
+}
+
+export const fetchSurveys = async (): Promise<Survey[]> => {
+    const response = await axios.get(process.env.NEXT_PUBLIC_API_URL + "/api/survey"); //esto deberia ser un parametro de configuracion
+    return response.data as Survey[]; // Se asume que la API devuelve datos en el formato esperado.
+};
