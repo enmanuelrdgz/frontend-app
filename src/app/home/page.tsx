e29615173606b254791aeeaee5f35385746cc6d3
Enmanuel Rodriguez <enmaundostres@gmail.com>
2025-03-18 21:26:22 -0300

    fix backend url

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -13,7 +13,7 @@ const HomePage: React.FC = () => {
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
-        axios.get("http://ec2-3-145-177-192.us-east-2.compute.amazonaws.com:8080" + "/poll")
+        axios.get("https://api.enmanuelrdgz.xyz" + "/poll")
             .then(res => {
                 setPolls(res.data.polls as PollData[]);
             })
--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -35,7 +35,7 @@ const AuthForm: React.FC<AuthFormProps> = ({action}) => {
             password: password
         }
 
-        axios.post("http://ec2-3-145-177-192.us-east-2.compute.amazonaws.com:8080" + action, body)
+        axios.post("https://api.enmanuelrdgz.xyz" + action, body)
             .then(res => {
                 signin(res.headers['token'])
                 router.push("/home")
