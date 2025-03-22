edfef54fdfa717b48b42540b51448f324eb35593
Conitoweb <daniel2905200@gmail.com>
2025-03-22 17:05:22 -0300

    refactor: code of the function

--- __tests__/calculatePercentage.js
+++
@@ -1,5 +0,0 @@
-import calculatePercentage from "../src/utils/calculatePercentage"
-
-test("Los porcentajes de votos se calculan correctamente", () => {
-    // que daniel piense en el test
-})
--- src/utils/calculatePercentage.ts
+++ src/utils/calculatePercentage.ts
@@ -1,7 +1,6 @@
 export default function calcultePercentage(option_votes: number, total_votes: number): number {
-    if(total_votes == 0) {
-        return 0
-    } else {
-        return ((option_votes / total_votes) * 100)
-    }
+// verifico si recibo params 
+if(!option_votes && !total_votes) NaN 
+    if(total_votes === 0) return total_votes // return 0
+    return (option_votes / total_votes) * 100 
 }
