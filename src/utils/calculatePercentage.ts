5284be27982f804918aa49269496d46041ee5a70
danielbenitez <daniel29052003@gmail.com>
2025-03-23 16:01:42 -0300

    remove condition of invalid parameters

--- src/utils/calculatePercentage.ts
+++ src/utils/calculatePercentage.ts
@@ -1,6 +1,4 @@
 export default function calcultePercentage(option_votes: number, total_votes: number): number {
-// verifico si recibo params 
-if(!option_votes && !total_votes) NaN 
-    if(total_votes === 0) return total_votes // return 0
+    if(total_votes === 0) return total_votes 
     return (option_votes / total_votes) * 100 
 }
