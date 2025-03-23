423088fb03948f20b02594fb84945fc7a0dd25ce
danielbenitez <daniel29052003@gmail.com>
2025-03-23 16:15:08 -0300

    added commet to describe + import describe

--- __tests__/calculatePercentage.js
+++ __tests__/calculatePercentage.js
@@ -1,5 +1,7 @@
-import {test} from "@jest/globals"
+import {test , describe} from "@jest/globals"
 import calculatePercentage from "../src/utils/calculatePercentage.ts"
+
+describe("calculatePercentage test suite" , ()=> {
 
     test("devolver cero si recibo solo un parametro con valor mayor a cero" , ()=>{
         const porcetaje = calculatePercentage(10 , 0)
@@ -9,12 +11,13 @@ import calculatePercentage from "../src/utils/calculatePercentage.ts"
     test("devolver cero si en ambos parametros recibo un numero igual a cero",()=>{
         const porcetaje = calculatePercentage(0 , 0)
         expect(porcetaje).toBe(0)
-       
+        
     })
     test("devolver el cien porciento si recibo dos numeros del mismo valor" , ()=>{
         const porcentaje = calculatePercentage(10,10)
         expect(porcentaje).toBe(100)
     }) 
- 
+    
+})
     
     
